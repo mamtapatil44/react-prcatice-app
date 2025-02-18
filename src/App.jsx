85d775581b44b4a3e26d9fd8665a9e763d421854
Mamta Patil <mamtapatil44@gmail.com>
2025-02-18 13:37:58 -0500

    tailwind css setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-        <div className='Container bg-red-500'>
-            <h1>Hellow tailwind css</h1>
+    <div className=''>
+        <div className='container bg-red-500 mx-auto'>
+            <h1 className='text-2xl text-white text-center'>Hellow tailwind css</h1>
         </div>
     </div>
   )
