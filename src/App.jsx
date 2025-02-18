bd5196ea369629b1658a1d9d315135c05082ab9b
Mamta Patil <mamtapatil44@gmail.com>
2025-02-18 13:24:00 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <div className='Container bg-red-500'>
+            <h1>Hellow tailwind css</h1>
+        </div>
+    </div>
+  )
+}
+
+export default App
